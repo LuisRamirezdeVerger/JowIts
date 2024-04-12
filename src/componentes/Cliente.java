7a0d118f71cbb7957bdb407cf2df57112f993bac
Wito R.d.V. <job.wito.uk@gmail.com>
2024-04-12 09:58:20 +0200

    creación clase menú y cliente

---
+++ src/componentes/Cliente.java
@@ -0,0 +1,12 @@
+package componentes;
+
+public class Cliente {
+
+    private int idCliente;
+    private String dniCliente;
+
+    public Cliente (int idCliente, String dniCliente){
+        this.idCliente = idCliente;
+        this.dniCliente = dniCliente;
+    }
+}
---
+++ src/componentes/Menu.java
@@ -0,0 +1,12 @@
+package componentes;
+
+public class Menu {
+
+    private int idMenu;
+    private int idCliente;
+
+    public Menu (int idMenu, int idCliente){
+        this.idMenu = idMenu;
+        this.idCliente = idCliente;
+    }
+}

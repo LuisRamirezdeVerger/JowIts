d15542a7c1db9d9578e7fd77f70fa6d5345f8586
JOSEMALEON <josem.leon.99@gmail.com>
2024-04-29 14:39:44 +0200

    Queda  por terminar la clase verProductos

--- src/InterfacesGraficas/RegistroNuevoEmpleado.java
+++ src/InterfacesGraficas/RegistroNuevoEmpleado.java
@@ -3,18 +3,30 @@ package interfacesGraficas;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class RegistroNuevoEmpleado extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -60,6 +72,103 @@ public class RegistroNuevoEmpleado extends JFrame {
 		lblImagen.setIcon(img2);
 		contentPane.add(lblImagen);
 		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nombre empleado:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setBounds(194, 376, 237, 43);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblDni = new JLabel("DNI:");
+		lblDni.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblDni.setBounds(194, 430, 237, 43);
+		contentPane.add(lblDni);
+		
+		JLabel lblTurnoQueTrabajara = new JLabel("Turno que trabajara:");
+		lblTurnoQueTrabajara.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblTurnoQueTrabajara.setBounds(194, 484, 237, 43);
+		contentPane.add(lblTurnoQueTrabajara);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.TRAILING);
+		textField.setBounds(798, 380, 237, 43);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		textField.setText("Añadir el nombre del nuevo empleado");
+		textField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				//Creamos evento que al hacer click, elimine el texto que haya.
+				textField.setText("");
+			}
+		});
+		
+		textField_1 = new JTextField();
+		textField_1.setHorizontalAlignment(SwingConstants.TRAILING);
+		textField_1.setColumns(10);
+		textField_1.setBounds(798, 434, 237, 43);
+		contentPane.add(textField_1);textField_1.setText("Añadir el DNI");
+		textField_1.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				//Creamos evento que al hacer click, elimine el texto que haya.
+				textField_1.setText("");
+			}
+		});
+		
+		textField_2 = new JTextField();
+		textField_2.setHorizontalAlignment(SwingConstants.TRAILING);
+		textField_2.setColumns(10);
+		textField_2.setBounds(798, 488, 237, 43);
+		contentPane.add(textField_2);textField_2.setText("En que turno trabajará");
+		textField_2.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				//Creamos evento que al hacer click, elimine el texto que haya.
+				textField_2.setText("");
+			}
+		});
+		
+		
+		
+		
+		/*
+		 * Boton que regista al empleado nuevo
+		 */
+		
+		 JButton btnRegistrar = new JButton("Registrar Empleado");
+	        btnRegistrar.setBounds(913, 131, 247, 102);
+	        contentPane.add(btnRegistrar);
+	        btnRegistrar.addActionListener(new ActionListener() {
+	           
+	        	@Override
+	        	public void actionPerformed(ActionEvent e) {
+	                // Obtener el texto de cada campo de texto
+	                String nombre = textField.getText();
+	                String dni = textField_1.getText();
+	                String turno = textField_2.getText();
+
+	                // Mostrar los datos en un cuadro de diálogo
+	                JOptionPane.showMessageDialog(contentPane,
+	                        "Nombre: " + nombre + "\nDNI: " + dni + "\nTurno: " + turno,
+	                        "Datos del Nuevo Empleado",
+	                        JOptionPane.INFORMATION_MESSAGE);
+	            }
+
+				
+	        });
+	    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
